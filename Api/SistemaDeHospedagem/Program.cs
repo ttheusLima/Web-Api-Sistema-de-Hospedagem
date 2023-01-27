fe97bfaf2e28878e4bfd4db6447447361bc6ddaa
ttheusLima <ttheus.rlima@gmail.com>
2023-01-27 16:06:02 -0300

    Adicionado injeção de Dependência, de ReservaService

--- Api/SistemaDeHospedagem/Program.cs
+++ Api/SistemaDeHospedagem/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SistemaDeHospedagem.Contexts;
+using SistemaDeHospedagem.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddTransient<ReservaService>();
 
 var app = builder.Build();
 
