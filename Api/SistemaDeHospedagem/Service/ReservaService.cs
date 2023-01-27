97da89c0d78daf82ee105a4ca2fccff3ba44c136
ttheusLima <ttheus.rlima@gmail.com>
2023-01-27 16:10:21 -0300

    Implementado os métodos da interface

--- Api/SistemaDeHospedagem/Service/ReservaService.cs
+++ Api/SistemaDeHospedagem/Service/ReservaService.cs
@@ -2,11 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaDeHospedagem.Contexts;
+using SistemaDeHospedagem.Interface;
+using SistemaDeHospedagem.Models;
 
 namespace SistemaDeHospedagem.Service
 {
-    public class ReservaService
+    public class ReservaService : IReservaService
     {
+        public ReservaService(){ }
         
+        private readonly HospedagemContext _context;
+        
+        public ReservaService(HospedagemContext context)
+        {
+            _context = context;
+        }
+
+        public void Post_Reserva(Reserva reserva)
+        {
+            _context.Reservas.Add(reserva);
+            _context.SaveChanges();
+        }
+
+        public Reserva GetReservaPorId(int id)
+        {
+            var reserva = _context.Reservas.Include(x => x.Hospedes)
+                                            .Include(x => x.Suite)
+                                            .FirstOrDefault(r => r.IdReserva == id);
+
+            return reserva;
+        }
+
+        public List<Reserva> Get_Produtos()
+        {
+            return _context.Reservas.ToList();
+        }
     }
 }
